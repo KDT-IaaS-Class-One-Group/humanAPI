993ab4faea86fb58d822447f8cf45b58a023da75
JungYoungSick <junfas1216@gmail.com>
2023-11-01 20:05:40 +0900

    서버 작성, 수정필요 접속 안됨

--- MapWeather/server.mjs
+++ MapWeather/server.mjs
@@ -1,12 +1,16 @@
 // app.mjs
 
 import express from 'express';
+import path from 'path'; // path 모듈 추가
 
 const app = express();
 const port = 7070;
 
+// 정적 파일 제공을 위한 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'MapWeather')));
+
 app.get('/', (req, res) => {
-  res.send('ESM Express 서버가 실행 중입니다.');
+  res.sendFile(path.join(__dirname, 'MapWeather', 'Map.html'));
 });
 
 app.listen(port, () => {
