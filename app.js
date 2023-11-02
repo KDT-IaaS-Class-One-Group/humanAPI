3707e11fe26396eb9cca69fd11fb0cf045d6cafc
csm <csm009177@gmail.com>
2023-11-02 17:13:24 +0900

    want make express server

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
+// express 보더 더 커스텀 하기 위한 http
 const http = require('http')
+// 기본적인 서빙을 위한 express
 const express = require('express')
-const app = express()
+// 간편하게 쓰기 위한 app 선언
+const app = express();
+// 파일 시스템을 쓰기위한 fs 선언
 const fs = require('fs');
+// 포트를 변수에 담음
 const port = 3217;
 
+//정적 파일을 서빙
 app.use(express.static("static"));
-// server
-app.get('/', (req, res) =>{
-  fs.readFile('')
+
+// 데이터를 파싱하기 위한 선언 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+// index res
+app.get('/login', (req, res) =>{
+  res.sendFile(__dirname + '/static/index.html')
 })
 
 
 
-serv.listen(port, () => {
+app.post('/', (req, res)=>{
+  const userid = req.body.userid;
+  const userpw = req.body.userpw;
+
+  const goodId = 'csm';
+  const goodPw = '0177';
+
+  if(userid === goodId && userpw=== goodPw){
+    res.sendFile(__dirname + '/static/search.html')
+  }
+})
+
+
+
+app.listen(port, () => {
   console.log(`
 http://localhost:${port}
   `)
 })
+
+
+
