ec347604205d68a183ba76c587206dc9a456dc45
JungYoungSick <junfas1216@gmail.com>
2023-11-13 11:58:40 +0900

    지도 zoomHandle 추가

--- MapWeather/staticMap/mapModule/MapApi.js
+++ MapWeather/staticMap/mapModule/MapApi.js
@@ -2,8 +2,13 @@ const MapApi = (mapDiv, map) => {
   mapDiv = document.getElementById("map");
   const mapOption = {
     center: new naver.maps.LatLng(36.34933, 127.3777),
+    scaleControl: false,
+    logoControl: false,
+    mapDataControl: false,
+    zoomControl: true,
     zoom: 15,
     mapTypeControl: true,
+
   };
 
   map = new naver.maps.Map("mapDiv", mapOption);
