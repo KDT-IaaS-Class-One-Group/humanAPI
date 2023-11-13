e766189f55a0dc23b2c94ec8df8226a49150746c
JungYoungSick <junfas1216@gmail.com>
2023-11-13 11:45:59 +0900

    미들웨어 설정 중 폴더명 변경

--- MapWeather/server.js
+++ MapWeather/server.js
@@ -1,12 +1,11 @@
 // app.mjs
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 7070;
 
 // 정적 파일 제공을 위한 미들웨어 설정
-app.use(express.static(path.join(__dirname, 'Map')));
+app.use(express.static(path.join(__dirname, 'staticMap')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'staticMap/Map.html'));
